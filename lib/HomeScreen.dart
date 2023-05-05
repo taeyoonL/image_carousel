6189930311cca5b991ff225fb96fb9eff5c18a54
taeyoonL <tylee3715@gmail.com>
2023-05-05 20:25:02 +0900

    image_carousel

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key : key);
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds : 3), (timer) {
+      int? nextPage = pageController.page?.toInt();
+      if(nextPage == null){
+        return;
+      }
+      if(nextPage == 4) {
+        nextPage = 0;
+      } else {
+        nextPage++;
+      }
+      pageController.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : PageView(
+        controller : pageController,
+        children: [1,2,3,4]
+            .map(
+            (number) => Image.asset(
+                'lib/asset/img/avengers_$number.jfif',
+                fit: BoxFit.cover,
+            ),
+        ).toList()
+      ),
+    );
+  }
+}
